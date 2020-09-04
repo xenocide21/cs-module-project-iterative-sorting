bb74dccd8b559de6b7c9a571b20959235164925c
Ashley Dunham <xenocide21@gmail.com>
2020-09-03 18:28:28 -0700

    iterative_sorting.py Complete (mvp)

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -9,6 +11,16 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    left = 0
+    right = len(arr) - 1
 
+    while left <= right:
+        mid = (right + left) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
 
     return -1  # not found
